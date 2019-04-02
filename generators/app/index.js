fcaa7811532af3687f48e4b2b1bd1b329317b39f
lyrieek <tianhao7@foxmail.com>
2019-04-02 22:53:33 +0800

    init project

---
+++ generators/app/index.js
@@ -0,0 +1,36 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+
+module.exports = class extends Generator {
+  prompting() {
+    this.log(
+      yosay(`Welcome to the supreme ${chalk.red('generator-gen')} generator!`)
+    );
+
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'someAnswer',
+        message: 'Would you like to enable this option?',
+        default: true
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      this.props = props;
+    });
+  }
+
+  writing() {
+    this.fs.copy(
+      this.templatePath('dummyfile.txt'),
+      this.destinationPath('dummyfile.txt')
+    );
+  }
+
+  install() {
+    this.installDependencies();
+  }
+};
